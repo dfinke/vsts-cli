5c3de49b2b27c2b2b4afdc37b076187a10e18d2d
dfinke <finked@hotmail.com>
2017-12-25 11:11:00 -0500

    updated

---
+++ scripts/DoIt.ps1
@@ -0,0 +1,29 @@
+﻿function New-PSVsts {
+    function upperFirst {
+        param([string]$target)
+        "{0}{1}" -f $target[0].ToString().ToUpper(), $target.Substring(1)
+    }
+
+    $cli="$args"
+    $functionName = "Invoke-Vsts"
+    $functionName += ($args | ForEach-Object {upperFirst ($_.replace("-","")) }) -join ''
+
+
+@"
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function $functionName {
+    param()
+
+    $("vsts {0}" -f $cli)
+}
+
+"@
+
+}
+
+python .\generate_ps_command.py | ForEach-Object { "New-PSVsts $($_)" | Invoke-Expression } | Set-Content .\vsts.psm1
---
+++ scripts/vsts.psm1
@@ -0,0 +1,432 @@
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsBuildList {
+    param()
+
+    vsts build list
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsBuildQueue {
+    param()
+
+    vsts build queue
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsBuildShow {
+    param()
+
+    vsts build show
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsBuildDefinitionList {
+    param()
+
+    vsts build definition list
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsBuildDefinitionShow {
+    param()
+
+    vsts build definition show
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsCodePrCreate {
+    param()
+
+    vsts code pr create
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsCodePrUpdate {
+    param()
+
+    vsts code pr update
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsCodePrShow {
+    param()
+
+    vsts code pr show
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsCodePrList {
+    param()
+
+    vsts code pr list
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsCodePrComplete {
+    param()
+
+    vsts code pr complete
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsCodePrAbandon {
+    param()
+
+    vsts code pr abandon
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsCodePrReactivate {
+    param()
+
+    vsts code pr reactivate
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsCodePrReviewersAdd {
+    param()
+
+    vsts code pr reviewers add
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsCodePrReviewersList {
+    param()
+
+    vsts code pr reviewers list
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsCodePrReviewersRemove {
+    param()
+
+    vsts code pr reviewers remove
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsCodePrWorkitemsAdd {
+    param()
+
+    vsts code pr work-items add
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsCodePrWorkitemsList {
+    param()
+
+    vsts code pr work-items list
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsCodePrWorkitemsRemove {
+    param()
+
+    vsts code pr work-items remove
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsCodePrSetvote {
+    param()
+
+    vsts code pr set-vote
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsCodePrPoliciesList {
+    param()
+
+    vsts code pr policies list
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsCodePrPoliciesQueue {
+    param()
+
+    vsts code pr policies queue
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsCodeRepoCreate {
+    param()
+
+    vsts code repo create
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsCodeRepoList {
+    param()
+
+    vsts code repo list
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsCodeRepoShow {
+    param()
+
+    vsts code repo show
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsLogin {
+    param()
+
+    vsts login
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsLogout {
+    param()
+
+    vsts logout
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsFeedback {
+    param()
+
+    vsts feedback
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsConfigure {
+    param()
+
+    vsts configure
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsProjectCreate {
+    param()
+
+    vsts project create
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsProjectShow {
+    param()
+
+    vsts project show
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsProjectList {
+    param()
+
+    vsts project list
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsWorkItemShow {
+    param()
+
+    vsts work item show
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsWorkItemCreate {
+    param()
+
+    vsts work item create
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsWorkItemQuery {
+    param()
+
+    vsts work item query
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsWorkItemUpdate {
+    param()
+
+    vsts work item update
+}
+
+<#
+    .SYNOPSIS
+      Short
+    .DESCRIPTION
+      Long
+#>
+function Invoke-VstsPsCvt {
+    param()
+
+    vsts ps cvt
+}
+
