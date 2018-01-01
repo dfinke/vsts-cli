c75fe0bf6da5df7badf1f1e17ff8dd6dbe0cdb5c
dfinke <finked@hotmail.com>
2018-01-01 11:30:51 -0500

    iex and save to psm1

--- scripts/DoIt.ps1
+++ scripts/DoIt.ps1
@@ -1,29 +1,71 @@
-﻿function New-PSVsts {
-    function upperFirst {
-        param([string]$target)
-        "{0}{1}" -f $target[0].ToString().ToUpper(), $target.Substring(1)
+﻿function Invoke-UpperCaseFirstLetter {
+    param([string]$target)
+
+    $target.Substring(0, 1).ToUpper() + $target.Substring(1)
+}
+
+function ConvertTo-PSFunction {
+    [CmdletBinding()]
+    param(
+        [Switch]$Evaluate,
+        [Switch]$SaveToPSM1
+    )
+
+    $r = python .\generate_ps_command.py | ConvertFrom-Csv | Group-Object command
+
+    $result = foreach ($record in $r) {
+        $parts = $record.name.split(' ')
+
+        $fnPart = switch ($parts.count) {
+            1 {"Vsts{0}" -f (Invoke-UpperCaseFirstLetter $parts[0])}
+            2 {"Vsts{0}" -f (Invoke-UpperCaseFirstLetter $parts[0])}
+            3 {"Vsts{0}{1}" -f (Invoke-UpperCaseFirstLetter $parts[0]), (Invoke-UpperCaseFirstLetter $parts[1])}
+            4 {"Vsts{0}{1}{2}" -f (Invoke-UpperCaseFirstLetter $parts[0]), (Invoke-UpperCaseFirstLetter $parts[1]), (Invoke-UpperCaseFirstLetter $parts[2])}
+        }
+
+        if ($fnPart) {
+            $DoConvertFromJson = $false
+            if ($parts.Count -eq 1) {
+                $functionName = "Invoke-{0}" -f ($fnPart -replace "-", "")
+            }
+            else {
+                $functionName = "{0}-{1}" -f (Invoke-UpperCaseFirstLetter $parts[-1]), ($fnPart -replace "-", "")
+                $DoConvertFromJson = $true
+            }
+
+            $record |
+                Add-Member -PassThru -MemberType NoteProperty -Name FunctionName -Value $functionName -Force |
+                Add-Member -PassThru -MemberType NoteProperty -Name DoConvertFromJson -Value $DoConvertFromJson -Force
+        }
     }
 
-    $cli="$args"
-    $functionName = "Invoke-Vsts"
-    $functionName += ($args | ForEach-Object {upperFirst ($_.replace("-","")) }) -join ''
+    $transpile = foreach ($record in $result | Sort-Object FunctionName) {
 
+        $targetExpression = "vsts $($record.name)"
+        if ($record.DoConvertFromJson) {
+            $targetExpression = "(vsts $($record.name) --output json | ConvertFrom-Json)"
+        }
+        $params = "`t$" + (($record.group.arg | Sort-Object) -join ",`n`t$")
 
-@"
-<#
-    .SYNOPSIS
-      Short
-    .DESCRIPTION
-      Long
-#>
-function $functionName {
-    param()
+        @"
+function $($record.FunctionName) {
+    param(
+$($params)
+    )
 
-    $("vsts {0}" -f $cli)
+    $targetExpression
 }
 
 "@
+    }
 
+    if($Evaluate) {
+        $transpile | Invoke-Expression
+    } elseif($SaveToPSM1) {
+        $fileName = "$pwd\vstsPS.psm1"
+        $transpile | Set-Content $fileName
+        Write-Verbose "Saved to $($fileName)"
+    } else {
+        $transpile
+    }
 }
-
-python .\generate_ps_command.py | ForEach-Object { "New-PSVsts $($_)" | Invoke-Expression } | Set-Content .\vsts.psm1
